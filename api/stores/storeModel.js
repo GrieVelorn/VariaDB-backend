2880fd0a50639cc5f83315afb4f72fa72f3f85a6
GrieVelorn <caseyvelorn@gmail.com>
2022-12-10 10:42:28 -0700

    Update storeModel.js

--- api/stores/storeModel.js
+++ api/stores/storeModel.js
@@ -1,23 +1,23 @@
 const db = require("../../data/knex.js");
 
 const findAll = async () => {
-  return await db("store").select("*");
+  return await db("stores").select("*");
 };
 
 const create = (ex) => {
-  return db("store").insert(ex).returning("*");
+  return db("stores").insert(ex).returning("*");
 };
 
 const findById = (id, val) => {
-  return db("store").where(id, "=", val).first().select("*");
+  return db("stores").where(id, "=", val).first().select("*");
 };
 
 const update = (id, val, ex) => {
-  return db("store").where(id, "=", val).first().update(ex).returning("*");
+  return db("stores").where(id, "=", val).first().update(ex).returning("*");
 };
 
 const remove = (id, val) => {
-  return db("store").where(id, "=", val).del();
+  return db("stores").where(id, "=", val).del();
 };
 
 module.exports = {
